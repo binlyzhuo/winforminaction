ffcd5b9320cbe1f09aa8717f95f39fc74db9d854
binlyzhuo <binlyzhuo@outlook.com>
2016-10-12 20:40:32 +0800

    ~

---
+++ winforminaction/MyPhotos/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPhotos
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open Photo";
+            dlg.Filter = "jpg files (*.jpg)|*.jpg|All files(*.*)|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pbxPhoto.Image = new Bitmap(dlg.OpenFile());
+            }
+            dlg.Dispose();
+        }
+    }
+}
