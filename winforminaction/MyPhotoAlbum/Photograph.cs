214e311a51050060a362240bdd2cd771e366c80f
binlyzhuo <binlyzhuo@outlook.com>
2016-10-12 21:35:03 +0800

    ~

---
+++ winforminaction/MyPhotoAlbum/Photograph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPhotoAlbum
+{
+    public class Photograph
+    {
+        private string _name;
+
+        public Photograph()
+        {
+            
+        }
+    }
+}
