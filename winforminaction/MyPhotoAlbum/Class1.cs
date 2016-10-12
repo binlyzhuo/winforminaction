770cb626b9c0b07117df612712f19290cf8a74e6
binlyzhuo <binlyzhuo@outlook.com>
2016-10-12 21:29:40 +0800

    ~

--- winforminaction/MyPhotoAlbum/Class1.cs
+++ winforminaction/MyPhotoAlbum/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyPhotos
+namespace MyPhotoAlbum
 {
-    class Photograph
+    public class Class1
     {
     }
 }
--- winforminaction/MyPhotoAlbum/Properties/AssemblyInfo.cs
+++ winforminaction/MyPhotoAlbum/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Photograph")]
+[assembly: AssemblyTitle("MyPhotoAlbum")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Photograph")]
+[assembly: AssemblyProduct("MyPhotoAlbum")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f4562dfe-0169-4389-a3fb-5410b8d233cf")]
+[assembly: Guid("efc38b88-e294-487a-9b36-1f274544e909")]
 
 // Version information for an assembly consists of the following four values:
 //
